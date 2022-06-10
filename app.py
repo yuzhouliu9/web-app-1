6a0d0edcb269939e74935004fe9823168f1dcae3
Yuzhou Liu <yuzhou.liu@mongodb.com>
2022-06-10 09:02:40 -0400

    deploy-test and change response to main app

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'web-app-1: /'
+    return 'main app: /'
 
 @app.route('/developer', strict_slashes=False) # matches /developer and /developer/
 def developer():
-    return 'web-app-1: /developer'
+    return 'main app: /developer'
 
 @app.route('/developer/sub/path', strict_slashes=False) # matches with trailing /
 def developer_sub_path():
-    return 'web-app-1: /developer/sub/path'
+    return 'main app: /developer/sub/path'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
