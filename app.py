1849c5b24ce4ccef7a3b2abcadd298bd3c1578da
Yuzhou Liu <yuzhou.liu@mongodb.com>
2022-06-10 10:49:56 -0400

    fix func names

--- app.py
+++ app.py
@@ -3,22 +3,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'main app: /'
+    return 'another app: /'
 
 @app.route('/developer-another-app', strict_slashes=False) # matches with trailing /
 def developer():
     return 'another app: /developer-another-app'
 
 @app.route('/developer-another-app/sub/path', strict_slashes=False)
-def developer_sub_path():
+def developer2():
     return 'another app: /developer-another-app/sub/path'
 
 @app.route('/developer/another-app', strict_slashes=False)
-def developer_sub_path():
+def developer3():
     return 'another app: /developer/another-app'
 
 @app.route('/developer/another-app/sub/path', strict_slashes=False)
-def developer_sub_path():
+def developer4():
     return 'another app: /developer/another-app/sub/path'
 
 if __name__ == '__main__':
